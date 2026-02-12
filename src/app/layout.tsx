decb4f5df5d9cf4477b230d0e95cd5dee25e0c0a
vc <vc@vcs-MacBook-Air.local>
2026-02-12 12:59:41 +0530

    updated-google-tag-manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17264874781"
+          strategy="afterInteractive"
+        />
+        <Script id="google-gtag" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17264874781');
+          `}
+        </Script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
